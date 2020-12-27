52b2cdb14517cea3143bb119d309a17f0b91e922
GimmiRuski <gimmiruski+github@gmail.com>
2020-12-27 02:56:05 +0100

    Change a few things

--- rencharmer.py
+++ rencharmer.py
@@ -1,100 +1,148 @@
 import tempfile
 
 import click
+import sh
+from rich.console import Console
+from rich.syntax import Syntax
 
+CONSOLE = Console()
 INDENTATION = "    "
 
 
 @click.command()
-@click.option("--file", is_flag=True)
+@click.option("--black", is_flag=True)
+@click.option("--print-python-blocks", is_flag=True)
 @click.argument("script", type=click.File())
-def main(file, script):
+def main(script, black, print_python_blocks):
     script = RenpyScript(script)
-    for python_block in script.python_blocks:
-        if file:
-            file_path = create_output_file(python_block)
-            click.echo(file_path)
-        else:
-            click.echo(python_block)
+    python_block_count = len(script.python_blocks)
+    plurality = "" if python_block_count == 1 else "s"
+    CONSOLE.log(f"Found {python_block_count} python block{plurality} in {script.path}")
+    for python_block_index in range(python_block_count):
+        python_block = script.python_blocks[python_block_index]
+        if black:
+            file_path = create_temporary_file(python_block)
+            CONSOLE.log(
+                f"Copied python block {python_block_index} contents into {file_path}"
+            )
+            sh.black("--target-version=py27", file_path)
+            CONSOLE.log(f"Formatted {file_path} with black")
+            modified_lines = read_temporary_file(file_path)
+            script.replace_python_block(python_block, modified_lines)
+            CONSOLE.log(
+                f"Replaced python block {python_block_index} in {script.path} with {file_path} contents"
+            )
+            script.save_changes()
+            CONSOLE.log(f"Saved {script.path} changes")
+        elif print_python_blocks:
+            CONSOLE.log(
+                f"Printing python block {python_block_index} from {script.path}"
+            )
+            code = str(python_block)
+            syntax = Syntax(code, "python", line_numbers=True)
+            CONSOLE.print(syntax)
 
 
-def create_output_file(python_block):
+def create_temporary_file(python_block):
     with tempfile.NamedTemporaryFile("w", delete=False, suffix=".py") as output_file:
-        output_file.writelines(python_block.lines)
+        indentation_level = python_block.indentation_level + 1
+        output_file.writelines(
+            line.content.replace(INDENTATION, "", indentation_level)
+            for line in python_block.lines
+        )
         return output_file.name
 
 
-class RenpyScript(object):
-    def __init__(self, script):
+def read_temporary_file(file_path):
+    lines = []
+    with open(file_path, "r") as file:
+        for line_index, line in enumerate(file):
+            line = RenpyScriptLine(line, line_index)
+            lines.append(line)
+    return lines
+
+
+class RenpyScript:
+    def __init__(self, file):
         self._lines = None
-        self._python_blocks = None
-        self._script = script
+        self._file = file
+
+    @property
+    def file(self):
+        return self._file
 
     @property
     def lines(self):
         if self._lines is None:
             self._lines = []
-            for index, line in enumerate(self.script):
+            for index, line in enumerate(self.file):
                 script_line = RenpyScriptLine(line, index)
                 self._lines.append(script_line)
         return self._lines
 
+    @lines.setter
+    def lines(self, lines):
+        for line_index, line in enumerate(lines):
+            line.index = line_index
+        self._lines = lines
+
     @property
     def path(self):
-        return self.script.name
+        return self.file.name
 
     @property
     def python_blocks(self):
-        if self._python_blocks is None:
+        block = None
+        python_blocks = []
+        for line in self.lines:
+            if "python:" in line.content:
+                block = PythonBlock(line.indentation_level)
+            elif block:
+                if line.is_empty or line.indentation_level > block.indentation_level:
+                    block.add_line(line)
+                else:
+                    python_blocks.append(block)
+                    block = None
+        if block:
+            python_blocks.append(block)
             block = None
-            self._python_blocks = []
-            for line in self.lines:
-                if "python:" in line.content:
-                    block = PythonBlock(line)
-                elif block:
-                    if (
-                        line.is_empty
-                        or line.indentation_level > block.indentation_level
-                    ):
-                        block.add_line(line)
-                    else:
-                        self._python_blocks.append(block)
-                        block = None
-            if block:
-                self._python_blocks.append(block)
-                block = None
-        return self._python_blocks
+        return python_blocks
 
-    @property
-    def script(self):
-        return self._script
+    def replace_python_block(self, python_block, modified_lines):
+        for line in modified_lines:
+            if not line.is_empty:
+                # We need to indent the line to match the original python
+                # block's indentation.
+                indentation = INDENTATION * (python_block.indentation_level + 1)
+                line.content = indentation + line.content
+        # Select the script's lines without including the old python block.
+        lines_first_half = self.lines[0 : python_block.first_line.index]
+        lines_second_half = self.lines[python_block.last_line.index + 1 :]
+        # Merge the lines together.
+        self.lines = lines_first_half + modified_lines + lines_second_half
+
+    def save_changes(self):
+        with open(self.path, "w") as file:
+            file.writelines(line.content for line in self.lines)
 
 
-class RenpyScriptLine(object):
+class RenpyScriptLine:
     def __init__(self, content, index):
-        self._content = content
-        self._index = index
-
-    @property
-    def content(self):
-        return self._content
+        self.content = content
+        self.index = index
 
     @property
     def indentation_level(self):
         return self.content.count(INDENTATION)
 
     @property
-    def index(self):
-        return self._index
-
-    @property
     def is_empty(self):
         return self.content == "\n"
 
 
-class PythonBlock(object):
-    def __init__(self, initialization_line):
-        self._indentation_level = initialization_line.indentation_level
+class PythonBlock:
+    def __init__(self, indentation_level):
+        self._indentation_level = indentation_level
         self.lines = []
 
     def __add__(self, other):
